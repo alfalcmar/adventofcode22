62ce85b3c37680356f6cc226330cb6c5575e8686
Alfonso Alcantara <a.alcantara.marin@gmail.com>
2023-01-05 19:44:55 +0100

    create a file to automatize days basic content

---
+++ createday.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+import os
+import argparse
+
+def create_hello_world_cpp(project_name):
+    # Create a new directory for the project
+    project_dir = project_name
+    os.makedirs(project_dir)
+
+    # Create the src directory
+    src_dir = os.path.join(project_dir, "src")
+    os.makedirs(src_dir)
+
+    # Create the CMakeLists.txt file
+    with open(os.path.join(project_dir, "CMakeLists.txt"), "w") as f:
+        f.write("""cmake_minimum_required(VERSION 3.10)
+project({project_name})
+
+set(CMAKE_CXX_STANDARD 20)
+
+# Add an executable target
+add_executable(hello_world src/main.cpp)
+        """.format(project_name=project_name))
+    # Create build file
+    with open(os.path.join(project_dir,"build.py"),"w") as f:
+        f.write("""#!/usr/bin/env python3
+
+import os
+
+def build_project():
+    # Get the path to the current directory
+    project_dir = os.getcwd()
+
+    # Create a build directory
+    build_dir = os.path.join(project_dir, "build")
+    os.makedirs(build_dir, exist_ok=True)
+
+    # Generate the makefiles
+    os.system(f"cmake {project_dir} -B{build_dir}")
+
+    # Build the project
+    os.system(f"cmake --build {build_dir}")
+
+# Example usage
+build_project()
+        """)
+    # give permission access
+    build_path = project_dir + "/build.py"
+    os.chmod(build_path, 0o777)
+
+    # Create the main.cpp file
+    with open(os.path.join(src_dir, "main.cpp"), "w") as f:
+        f.write("""#include <iostream>
+#include<ranges>
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+        """)
+    # Create a txt file
+    filename = project_dir + "/input.txt"
+    open(filename, "w").close()
+
+    # Print a message
+    print(f"Successfully created project {project_name}!")
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("project_name", help="name of the project to create")
+args = parser.parse_args()
+
+# Create the project
+create_hello_world_cpp(args.project_name)
+
