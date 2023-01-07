be02a255be7ee3c60e41aa4b7569a4a7068f5310
Alfonso AÃlcantara <a.alcantara.marin@gmail.com>
2023-01-07 10:36:49 +0100

    part2 finished

--- day1/src/main.cpp
+++ day1/src/main.cpp
@@ -1,57 +1,56 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
-#include<ranges>
-#include<string>
-#include<utility>
-#include<fstream>
-#include<vector>
-#include<algorithm>
 #include <numeric>
+#include <ranges>
+#include <string>
+#include <utility>
+#include <vector>
 
+void part1(const std::vector<std::string> &input) {
+  auto calories_by_elf =
+      input | std::views::lazy_split("") |
+      std::views::transform([](const auto &in) -> int {
+        auto to_int = [](const auto &s) { return std::stoi(s); };
+        auto by_elf = in | std::views::transform(to_int) | std::views::common;
 
-void part1(const std::vector<std::string> &input){
-  auto calories_by_elf = input | std::views::lazy_split("") 
-    | std::views::transform([](const auto &in) -> int {
-        auto to_int = [](auto s){return std::stoi(s);};
-        auto by_elf = in | std::views::transform(to_int) |
-        std::views::common;
-    
-    return std::reduce(by_elf.begin(), by_elf.end());
+        return std::reduce(by_elf.begin(), by_elf.end());
+      });
+  auto it = std::ranges::max_element(calories_by_elf);
 
-    });
-    auto it = std::ranges::max_element(calories_by_elf);
-
-   std::cout<<*it<<std::endl;
-
+  std::cout << *it << std::endl;
 }
 
-void part2(const std::vector<std::string> &input){
-  // auto calories_by_elf = input | std::views::lazy_split("") 
-  //   | std::views::transform([](auto in) -> int {
-  //       auto to_int = [](auto s){return std::stoi(s);};
-  //       auto by_elf = in | std::views::transform(to_int);
-    
-  //   return std::reduce(by_elf.begin(), by_elf.end(), std::greater());
+void part2(const std::vector<std::string> &input) {
+  auto calories_by_elf =
+      input | std::views::lazy_split("") |
+      std::views::transform([](const auto &in) -> int {
+        auto to_int = [](const auto &s) { return std::stoi(s); };
+        auto by_elf = in | std::views::transform(to_int) | std::views::common;
 
-  //   });
-  // std::cout<<std::ranges::max_element(calories_by_elf)<<std::endl;
-
+        return std::reduce(by_elf.begin(), by_elf.end());
+      });
+  std::vector<int> result(3);
+  std::ranges::partial_sort_copy(calories_by_elf, result, std::greater<>());
+  auto sum = std::reduce(result.begin(), result.end());
+  std::cout << "part2 result: " << sum << std::endl;
 }
 
 int main() {
-    std::vector<std::string> lines; 
-    std::string line;
-    std::ifstream file("../input.txt");
-    if (file.is_open()) {
-        while (std::getline(file, line)) {
-            lines.push_back(std::move(line));
-        }
-        file.close();
-    } else {
-        std::cout<< "file could not be opened"<<std::endl;
+  std::vector<std::string> lines;
+  std::string line;
+  std::ifstream file("../input.txt");
+  if (file.is_open()) {
+    while (std::getline(file, line)) {
+      lines.push_back(std::move(line));
     }
+    file.close();
+  } else {
+    std::cout << "file could not be opened" << std::endl;
+  }
 
-    part1(lines);
+  part1(lines);
+  part2(lines);
 
-    return 0;
+  return 0;
 }
-        
